9fee3a9b652a19e06a5c3ba0d87bd9d5ee6929b6
Colin Sherratt <colin.sherratt@gmail.com>
2013-11-01 22:56:24 -0400

    added create_compute_context_prefer

--- util.rs
+++ util.rs
@@ -18,6 +18,41 @@ pub fn create_compute_context() -> Result<(Device, Context, CommandQueue), ~str>
     Ok((devices[0], context, queue))
 }
 
+pub enum PreferedType {
+    ANY,
+
+    CPU_PREFERED,
+    GPU_PREFERED,
+
+    CPU_ONLY,
+    GPU_ONLY,
+
+}
+
+pub fn create_compute_context_prefer(cltype: PreferedType) -> Result<(Device, Context, CommandQueue), ~str>
+{
+    let platforms = get_platforms();
+    for platform in platforms.iter() {
+        let types = match cltype {
+            ANY => &[CPU, GPU],
+            CPU_PREFERED | CPU_ONLY => &[CPU],
+            GPU_PREFERED | GPU_ONLY => &[GPU]
+        };
+
+        let devices = platform.get_devices_by_types(types);
+        if devices.len() != 0 {
+            let context = devices[0].create_context();
+            let queue = context.create_command_queue(&devices[0]);
+            return Ok((devices[0], context, queue));
+        } 
+    }
+
+
+    match cltype {
+        ANY | CPU_PREFERED | GPU_PREFERED => create_compute_context(),
+        _ => Err(~"Could not find valid implementation")    
+    }
+}
 
 #[cfg(test)]
 pub fn test_all_platforms_devices(test: &fn(Device, Context, CommandQueue))
